ad057372b2b848853a299ac3307cade15775aecd
Joey <jvanhoek@hotmail.com>
2018-04-20 12:47:37 +0200

    TIen propperties

--- TabletStart.h
+++ TabletStart.h
@@ -1,11 +1,18 @@
 #pragma once
 #include <VrLib\Application.h>
+#include <VrLib\tien\Tien.h>
 #include <glm\glm.hpp>
 
 class TabletStart : public vrlib::Application
 {
+	//vrlib::DigitalDevice leftbutton;
+private:
+	vrlib::tien::Tien Engine;
+
 public:
 	virtual void init() override;
+	virtual void preFrame(double frameTime, double totalTime) override;
+	virtual void latePreFrame() override;
 	virtual void draw(const glm::mat4 &projectionMatrix, const glm::mat4 &modelViewMatrix) override;
 
 	TabletStart();
