804a27be7f6c388fcb9fd0b06f12a6198c4f43a6
fbelzeli@student.avans.nl <fbelzeli@student.avans.nl>
2018-04-20 14:16:25 +0200

    tablet basic setup

--- Tablet.cpp
+++ Tablet.cpp
@@ -1,8 +1,23 @@
 #include "stdafx.h"
 #include "Tablet.h"
 
+#include <VrLib\tien\components\TransformAttach.h>
+
+using namespace vrlib;
 using namespace vrlib::tien;
 
-Tablet::Tablet(components::Transform& leftHand, components::Transform& rightHand) : Node("Tablet",nullptr) {
+Tablet::Tablet(PositionalDevice& leftHand, PositionalDevice& rightHand) {
+	
+}
+
+void Tablet::update(float elapsedTime, Scene& scene) {
+
+}
+
+void Tablet::drawDeferredPass() {
+
+}
+
+void Tablet::postUpdate(Scene& scene) {
 
 }
--- Tablet.h
+++ Tablet.h
@@ -2,9 +2,14 @@
 
 #include "stdafx.h"
 #include <VrLib\tien\Tien.h>
+#include <VrLib\Device.h>
 
-class Tablet : public vrlib::tien::Node {
+class Tablet : public vrlib::tien::components::Renderable {
 public:
 
-	Tablet(vrlib::tien::components::Transform& leftHand, vrlib::tien::components::Transform& rightHand);
+	Tablet(vrlib::PositionalDevice& leftHand, vrlib::PositionalDevice& rightHand);
+
+	void update(float elapsedTime, Scene& scene) override;
+	void drawDeferredPass() override;
+	void postUpdate(Scene& scene) override;
 };
