5b830d04023bb2b9cda6e2e418626dcccdc2e3e0
fbelzeli@student.avans.nl <fbelzeli@student.avans.nl>
2018-04-20 12:46:50 +0200

    added Tablet

---
+++ Tablet.cpp
@@ -0,0 +1,8 @@
+#include "Tablet.h"
+
+using namespace vrlib::tien;
+
+
+/*Tablet::Tablet(components::Transform& leftHand, components::Transform& rightHand) : Node("Tablet",nullptr) {
+
+}*/
---
+++ Tablet.h
@@ -0,0 +1,10 @@
+#pragma once
+
+#include "stdafx.h"
+//#include <VrLib\tien\Tien.h>
+
+class Tablet /*: public vrlib::tien::Node*/ {
+public:
+
+	//Tablet(vrlib::tien::components::Transform& leftHand, vrlib::tien::components::Transform& rightHand);
+};
